f90172871a2db02a8fb40da0d2ae03753711df33
Nathan <nathanfrad86@gmail.com>
2019-04-25 12:56:49 +0200

    sans jquerry + nouveau logo

--- script.js
+++ script.js
@@ -1,20 +1,25 @@
-
 document.addEventListener("DOMContentLoaded", function (event) {
 
-let ancienAudio = null
-    $("a").click(function () {
-        if ( ancienAudio != null){
+    let ancienAudio = null
+
+    var buttons = document.getElementsByTagName("a")
+
+    for (var i = 0; i < buttons.length; i++) {
+        buttons[i].onclick = playSound;
+    }
+
+    function playSound() {
+        if (ancienAudio != null) {
             ancienAudio.pause()
             ancienAudio.currentTime = 0;
         }
         let id = this.id;
         let audio = document.getElementsByClassName(id)[0];
-        if(id === "goal"){
-            audio.volume= 0.5;
+        if (id === "goal") {
+            audio.volume = 0.5;
         }
-
         audio.play();
         ancienAudio = audio;
+    };
 
-    });
 });
